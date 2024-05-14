61bc0d2e3d2ed6128add0ba71adcfa6a2768cd7c
Efstratiou2001 <167864141+Efstratiou2001@users.noreply.github.com>
2024-05-14 18:04:30 +0300

    Update morse.ino

--- arduino code/3_morse_code/morse/morse.ino
+++ arduino code/3_morse_code/morse/morse.ino
@@ -26,16 +26,6 @@
 // LED (+) to pin 13, (-) 220 Ohm resistor to ground
 //
 
-#include <SPI.h>
-#include <RF22.h>
-#include <RF22Router.h>
-
-#define MY_ADDRESS 3 // define my unique address
-#define DESTINATION_ADDRESS_1 0 // define who I can talk to
-
-
-// Singleton instance of the radio
-RF22Router rf22(MY_ADDRESS); // initiate the class to talk to my radio with MY_ADDRESS
 
 
 int received_value=0;
@@ -89,41 +79,6 @@ void setup()
   pinMode(buttonPin, INPUT_PULLUP);
   Serial.begin(9600);
 
-  \\Rx code 
-if (!rf22.init())
-    Serial.println("RF22 init failed");
-  // Defaults after init are 434.0MHz, 0.05MHz AFC pull-in, modulation FSK_Rb2_4Fd36
-  if (!rf22.setFrequency(438.0)) // The frequency should be the same as that of the transmitter. Otherwise no communication will take place
-    Serial.println("setFrequency Fail");
-  rf22.setTxPower(RF22_TXPOW_20DBM);
-  //1,2,5,8,11,14,17,20 DBM
-  rf22.setModemConfig(RF22::OOK_Rb40Bw335  );// The modulation should be the same as that of the transmitter. Otherwise no communication will take place
-  //modulation
-
-  // Manually define the routes for this network
-  rf22.addRouteTo(DESTINATION_ADDRESS_3, DESTINATION_ADDRESS_3); // tells my radio card that if I want to send data to DESTINATION_ADDRESS_1 then I will send them directly to DESTINATION_ADDRESS_1 and not to another radio who would act as a relay 
- delay(1000);
-  
-  \\Tx code
-  
-if (!rf22.init()) // initialize my radio
-    Serial.println("RF22 init failed");
-  // Defaults after init are 434.0MHz, 0.05MHz AFC pull-in, modulation FSK_Rb2_4Fd36
-  if (!rf22.setFrequency(439.0)) // set the desired frequency
-    Serial.println("setFrequency Fail");
-  rf22.setTxPower(RF22_TXPOW_20DBM); // set the desired power for my transmitter in dBm
-  //1,2,5,8,11,14,17,20 DBM
-  rf22.setModemConfig(RF22::OOK_Rb40Bw335  ); // set the desired modulation
-  //modulation
-
-  // Manually define the routes for this network
-  rf22.addRouteTo(DESTINATION_ADDRESS_3, DESTINATION_ADDRESS_3);
- 
-  delay(1000);
-  
-
-
-  
   Serial.println();
   Serial.println("-------------------------------");
   Serial.println("Morse Code decoder/encoder");
