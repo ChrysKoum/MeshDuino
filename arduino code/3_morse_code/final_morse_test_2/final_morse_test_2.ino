58e8a822ce5fbe64c1c90fe3ca48a1d2752d0281
Chrysostomos Koumides <chyrkoum@gmail.com>
2024-05-16 14:04:40 +0300

    Refactor Arduino code for final morse test

--- arduino code/3_morse_code/final_morse_test_2/final_morse_test_2.ino
+++ arduino code/3_morse_code/final_morse_test_2/final_morse_test_2.ino
@@ -8,12 +8,72 @@
 
 RF22Router rf22(MY_ADDRESS); // Initiate the RF22 with the sender's address
 
-void setup() {
+int tonePin = 2;
+int toneFreq = 1000;
+int ledPin = 13;
+int buttonPin = 8;
+int debounceDelay = 90;
+
+int dotLength = 240;
+// dotLength = basic unit of speed in milliseconds
+// 240 gives 5 words per minute (WPM) speed.
+// WPM = 1200/dotLength.
+// For other speeds, use dotLength = 1200/(WPM)
+//
+// Other lengths are computed from dot length
+int dotSpace = dotLength;
+int dashLength = dotLength * 4;
+int letterSpace = dotLength * 3;
+int wordSpace = dotLength * 7;
+float wpm = 1200. / dotLength;
+
+int t1, t2, onTime, gap;
+bool newLetter, newWord, letterFound, keyboardText;
+int lineLength = 0;
+int maxLineLength = 20;
+
+char *letters[] =
+    {
+        ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", // A-I
+        ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", // J-R
+        "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};       // S-Z
+
+char *numbers[] =
+    {
+        "-----", ".----", "..---", "...--", "....-", // 0-4
+        ".....", "-....", "--...", "---..", "----." // 5-9
+    };
+
+String dashSeq = "";
+char keyLetter, ch;
+int i, index;
+String decodedLetters = ""; // Holds all decoded letters
+int number_of_bytes = 0;    // will be needed to measure bytes of message
+
+float throughput = 0; // will be needed for measuring throughput
+int flag_measurement = 0;
+
+int counter = 0;
+int initial_time = 0;
+int final_time = 0;
+bool k = true;
+
+void setup()
+{
     delay(500);
+    pinMode(ledPin, OUTPUT);
+    pinMode(tonePin, OUTPUT);
+    pinMode(buttonPin, INPUT_PULLUP);
     Serial.begin(9600);
     Serial.println();
     Serial.println("-------------------------------");
     Serial.println("Morse Code decoder/encoder");
+    Serial.print("Speed=");
+    Serial.print(wpm);
+    Serial.print("wpm, ");
+    Serial.print("dot=");
+    Serial.print(dotLength);
+    Serial.println("ms");
 
     // Rx,Tx code
     if (!rf22.init()) // initialize my radio
@@ -24,12 +84,36 @@ void setup() {
     rf22.setTxPower(RF22_TXPOW_20DBM); // set the desired power for my transmitter in dBm
     // 1,2,5,8,11,14,17,20 DBM
     rf22.setModemConfig(RF22::OOK_Rb40Bw335); // set the desired modulation
+    // modulation
 
     // Manually define the routes for this network
     rf22.addRouteTo(DESTINATION_ADDRESS_1, DESTINATION_ADDRESS_1); // tells my radio card that if I want to send data to DESTINATION_ADDRESS_1 then I will send them directly to DESTINATION_ADDRESS_1 and not to another radio who would act as a relay
     delay(1000); // delay for 1 s
 
-    Serial.println("Setup complete.");
+    // Test the LED and tone
+    tone(tonePin, toneFreq);
+    digitalWrite(ledPin, HIGH);
+    delay(2000);
+    digitalWrite(ledPin, LOW);
+    noTone(tonePin);
+    delay(600);
+
+    // flash to demonstrate the expected key speed
+    // A
+    Serial.print("A .-  ");
+    index = 'A' - 65;
+    flashSequence(letters[index]);
+    delay(wordSpace);
+    Serial.println();
+    Serial.println("-------------------------------");
+    Serial.println("Click field in Serial Monitor,");
+    Serial.println("type text and press Enter, or");
+    Serial.println("Key in Morse Code to decode:");
+    Serial.println("-------------------------------");
+
+    newLetter = false; // if false, do NOT check for end of letter gap
+    newWord = false;   // if false, do NOT check for end of word gap
+    keyboardText = false;
 }
 
 void loop() {
